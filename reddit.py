9108dbcb9a5f2fe66b595560bccfa332318a55a7
Ali Abdoli <xXAligatorXx@outlook.com>
2018-07-02 11:51:39 -0400

    added randomness

--- reddit.py
+++ reddit.py
@@ -2,25 +2,27 @@
 
 import praw
 import time
+import random
 
 # reddit api login
-reddit = praw.Reddit(client_id='Rh_qf9ARRcVwxw',
-                     client_secret='DoFKTulIjR85XhlGuDhnrJdCTc4',
+reddit = praw.Reddit(client_id='API INFO',
+                     client_secret='API INFO',
                      username='ThanosBanBot',
                      password='PASSWORD',
-                     user_agent='ThanosBot 1.0')
+                     user_agent='ThanosBot 1.1')
 
-Subreddit = reddit.subreddit("thanosdidnothingwrong")
+Subreddit = reddit.subreddit("modabuse")
 maximum = Subreddit.subscribers
 
 def banHalf():
     counter = 0;
     while True:
         for comment in Subreddit.comments(limit=1):
+            Random = random.randint(0,1)
             if comment.author_flair_text != 'survived' and comment.author_flair_text != 'perished':
-                if counter % 2 == 0:
+                if Random == 0:
                     Subreddit.flair.set(comment.author.name,'survived')
-                else:
+                if Random == 1:
                     Subreddit.flair.set(comment.author.name,'perished')
                     Subreddit.banned.add(comment.author.name, ban_reason='The universe needs balance')
                 time.sleep(3)
