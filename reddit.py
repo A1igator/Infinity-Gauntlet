5961b2a1da86c0f046456cc5dcaaaa03035a9e01
Ali Abdoli <xXAligatorXx@outlook.com>
2018-07-02 12:14:11 -0400

    added posts as well

--- reddit.py
+++ reddit.py
@@ -9,7 +9,7 @@ reddit = praw.Reddit(client_id='API INFO',
                      client_secret='API INFO',
                      username='ThanosBanBot',
                      password='PASSWORD',
-                     user_agent='ThanosBot 1.1')
+                     user_agent='ThanosBot 1.2')
 
 Subreddit = reddit.subreddit("modabuse")
 maximum = Subreddit.subscribers
@@ -17,7 +17,7 @@ maximum = Subreddit.subscribers
 def banHalf():
     counter = 0;
     while True:
-        for comment in Subreddit.comments(limit=1):
+        for comment in Subreddit.comments():
             Random = random.randint(0,1)
             if comment.author_flair_text != 'survived' and comment.author_flair_text != 'perished':
                 if Random == 0:
@@ -27,6 +27,18 @@ def banHalf():
                     Subreddit.banned.add(comment.author.name, ban_reason='The universe needs balance')
                 time.sleep(3)
                 counter+= 1
-                if counter == maximum:
-                    return
+        for post in Subreddit.new():
+            print(post.author.name)
+            Random = random.randint(0,1)
+            if post.author_flair_text != 'survived' and post.author_flair_text != 'perished':
+                if Random == 0:
+                    Subreddit.flair.set(post.author.name,'survived')
+                if Random == 1:
+                    Subreddit.flair.set(post.author.name,'perished')
+                    Subreddit.banned.add(post.author.name, ban_reason='The universe needs balance')
+                time.sleep(3)
+                counter+= 1
+        if counter == maximum:
+            return
 banHalf()
+
